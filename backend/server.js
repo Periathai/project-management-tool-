f13988730191c5c76ba7f632a163325482108f21
Periathai <periathai78@gmail.com>
2026-01-01 19:20:00 +0530

    Initialize Express server with routing and MongoDB

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.js";
+import projectRoutes from "./routes/project.js";
+import taskRoutes from "./routes/task.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+app.use("/auth", authRoutes);
+app.use("/projects", projectRoutes);
+app.use("/tasks", taskRoutes);
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
